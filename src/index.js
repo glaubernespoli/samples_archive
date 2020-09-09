5fb5e87c956211ccdf0f2972901f6e0b6828369a
glaubernespoli <glauber.nespoli@gmail.com>
2020-09-09 14:25:13 +0200

    overlapping cards

--- src/index.js
+++ src/index.js
@@ -7,11 +7,20 @@ const PORT = 5000;
 const server = express();
 const lrServer = livereload.createServer();
 const publicPath = path.join(__dirname, '../docs');
+const htmlFiles = path.join(publicPath, 'samples');
 
 server.use(connectLR());
 server.use(express.static(publicPath));
 lrServer.watch(publicPath);
 
+server.get('/timeline-scroll', (req, res) => {
+        res.sendFile(path.join(htmlFiles, 'timeline-scroll.html'));
+    })
+    .get('/overlapping-cards', (req, res) => {
+        res.sendFile(path.join(htmlFiles, 'overlapping-cards.html'));
+    });
+
+
 server.listen(PORT, () => {
-	console.log(`Server up on port ${PORT}.`);
+    console.log(`Server up on port ${PORT}.`);
 });
